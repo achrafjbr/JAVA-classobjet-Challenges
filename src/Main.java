14a5327dbe82303f54cba7cacf28009ff353541e
saraouhra <saraouhra02@gmail.com>
2025-11-11 11:26:17 +0100

    Adding the Rectangle class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // new Student("Ahmad",20, 32).displayInfo();
         // new Product(101, 8500.0,  "Laptop" ).displayProduct();
-
+        //Rectangle rectangle = new Rectangle(5, 10);
+        //rectangle.calculateArea();
     }
 }
