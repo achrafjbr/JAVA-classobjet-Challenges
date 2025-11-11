b0fbfc44fcdc9de925b4f0467f0feee9fdc0958a
saraouhra <saraouhra02@gmail.com>
2025-11-11 11:39:01 +0100

    Adding BankAccount class

---
+++ src/BankAccount.java
@@ -0,0 +1,21 @@
+public class BankAccount {
+    int accountNumber;
+    double balance;
+
+    public BankAccount(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void displayBalance() {
+        System.out.println("Your Balance : " + balance + " ");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /// Student
         // new Student("Ahmad",20, 32).displayInfo();
         // new Product(101, 8500.0,  "Laptop" ).displayProduct();
+
+        ///  Rectangle
         //Rectangle rectangle = new Rectangle(5, 10);
         //rectangle.calculateArea();
         //rectangle.display();
+
+        /// BankAccount
+       // BankAccount bankAccount = new BankAccount(1,1000);
+         /// deposit
+        // bankAccount.deposit(200);
+        // bankAccount.displayBalance();
+         /// withdraw
+        // bankAccount.withdraw(500);
+        // bankAccount.displayBalance();
+
+
+
+
     }
 }
